cb4850becbe31803d03b890ec0c644805c61563b
lyupeiyang <1714087802@qq.com>
2025-06-24 14:15:40 +0000

    contriever3

---  encoding.py
+++  encoding.py
@@ -9,7 +9,7 @@ def encode_and_save_documents(doc_texts, doc_ids, memmap_path, csv_map_path):
     # 实例化模型
     model = Contriever(model_hgf='facebook/contriever-msmarco')
 
-    print("✅ 开始编码文档，总量：", len(doc_texts))
+    print(" 开始编码文档，总量：", len(doc_texts))
 
     # 编码
     document_embeddings = model.encode_documents(doc_texts)
@@ -30,8 +30,8 @@ def encode_and_save_documents(doc_texts, doc_ids, memmap_path, csv_map_path):
     df = pd.DataFrame({"doc_id": doc_ids, "offset": offsets})
     df.to_csv(csv_map_path, index=False)
 
-    print(f"✅ 编码完成！已保存 memmap 至：{memmap_path}")
-    print(f"✅ 映射表已保存至：{csv_map_path}")
+    print(f" 编码完成！已保存 memmap 至：{memmap_path}")
+    print(f" 映射表已保存至：{csv_map_path}")
 
 
 if __name__ == "__main__":
@@ -41,7 +41,7 @@ if __name__ == "__main__":
     doc_ids = []
     doc_texts = []
 
-    #print("📦 开始读取文档内容...")
+    #print(" 开始读取文档内容...")
 
     # 只读取前 100000 条文档
     #for i, doc in enumerate(dataset.docs_iter()):
@@ -50,19 +50,19 @@ if __name__ == "__main__":
         #doc_ids.append(doc.doc_id)
         #doc_texts.append(doc.text)
 
-    #print(f"📈 已读取文档数：{len(doc_ids)}")
+    #print(f" 已读取文档数：{len(doc_ids)}")
 
-    print("📦 开始读取文档内容...")
+    print(" 开始读取文档内容...")
 
     for doc in dataset.docs_iter():
         doc_ids.append(doc.doc_id)
         doc_texts.append(doc.text)
 
-    print(f"📈 总文档数：{len(doc_ids)}")
+    print(f" 总文档数：{len(doc_ids)}")
     # 输出路径（可根据项目结构调整）
     memmap_path = "data/memmap/msmarco-passages/Contriever/Contriever.dat"
     csv_map_path = "data/memmap/msmarco-passages/Contriever/Contriever_map.csv"
 
     encode_and_save_documents(doc_texts, doc_ids, memmap_path, csv_map_path)
 
-    print("🎉 All done.")
+    print(" All done.")
